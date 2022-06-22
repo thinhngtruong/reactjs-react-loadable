83473efb6607c38535f69917e3e66a469ba085d5
Thinh Nguyen <thinh.ngtruong@gmail.com>
2022-06-22 23:00:55 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Link, Router } from "@reach/router";
+import Loadable from "react-loadable";
+
+import "./App.css";
+
+const LoadableAbout = Loadable({
+  loader: () => import("./About"),
+  loading() {
+    return <h1>Loading split out code...</h1>;
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Link to="about">About Us</Link>
+      <Router>
+        <LoadableAbout path="about" />
+      </Router>
+    </React.Fragment>
   );
 }
 
